94aafd6ffa26ba63800e7d28a86526ae21ce3cf7
Tomas Alvarez <tomasac2503@gmail.com>
2026-05-05 08:24:33 -0500

    Fix-codificación del texto para que pueda mostrar todos los carácteres

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from proyecto.agent.loop import run_agent
 from proyecto.mcp_server.db.session import create_tables
 
+
 def prompt(message:str):
     print("Enviando mensaje al agente...")
     response = run_agent(message)
-    print("Respuesta del agente:", response)
+    print("Respuesta del agente:", fix_response(response))
+    
+def fix_response(response:str):
+    if "Ã" in response:
+        try:
+            return response.encode('latin1').decode('utf-8')
+        except UnicodeDecodeError:
+            pass
+    return response
 
 if __name__ == "__main__":
     create_tables()
